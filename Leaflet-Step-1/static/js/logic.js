f9e1d99dd088b784964c17fbd9bc27cfc3750d79
Jade <jadeve.jimenez@yahoo.com>
2021-05-03 12:31:45 -0700

    map won't show up

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,8 +4,62 @@ d3.json(queryUrl).then(function(response){
     //console.log(response)
     features = response.features
 
-    function onEachFeature(featureData,layer){
+    function featurePop(featureData,layer){
         layer.bindPopup("<h3>"+featureData.properties.place +
         "</h3><hr><p>"+ new Date(featureData.properties.time)+"</p>")
     }
+    var earthquakes = L.geoJSON(features, {
+        onEachFeature: featurePop,
+    
+        pointToLayer: (featureData, latlng) => 
+        {
+          //console.log(featureData)
+          //return L.marker(latlng)
+          
+          return L.circle(latlng,
+            {radius: featureData.properties.mag*10000}
+            )
+        }
+    });
+    
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
 })
+
+
+
