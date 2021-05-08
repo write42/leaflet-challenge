1cceb865faa259b4279a81df684190367fa26c17
Jade <jadeve.jimenez@yahoo.com>
2021-05-07 20:24:53 -0700

    stuck on making legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -92,13 +92,23 @@ d3.json(queryUrl).then(function(response){
     collapsed: false
   }).addTo(myMap);
 
-  var info = L.control({
+  var legend = L.control({
     position:'bottomright'
   });
 
-  info.onAdd = function(map){
+  legend.onAdd = function(myMap){
     var div = L.DomUtil.create('div','legend');
+    labels = ['<strong>Depth</strong>']
+    categories = ['-10-10','10-30','30-50','50-70','70-90','90+'];
+    colors = ['#00FF00','#B3FF00','#F0FF00','#FFB100','#FF8300','#FF1300']
+    for (var i=0; i < categories.length; i++) {
+      div.innerHTML +=
+      labels.push(
+        '<i class = "circle" style = "background:' + colors[i] + '"></i>' + (categories[i] ? categories[i] : '+')
+      );
+    }
+    div.innerHTML = labels.join('<br>')
     return div;
   };
-  info.addTo(myMap);
+  legend.addTo(myMap);
 })
