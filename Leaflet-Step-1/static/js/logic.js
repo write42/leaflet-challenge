26303801e31f5472829088dac7979094adc44152
Jade <jadeve.jimenez@yahoo.com>
2021-05-08 15:04:29 -0700

    colors won't print correctly

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,15 @@
+function getColor(c){
+  return c > -10 ? '#00FF00':
+    c > 10 ? '#B3FF00':
+    c > 30 ? '#F0FF00':
+    c > 50 ? '#FFB100':
+    c > 70 ? '#FF8300':
+    c > 90 ? '#FF1300':
+            '#000000';
+}
+
+
+
 function circleColor(mag) {
   var color = "";
   switch (true) {
@@ -97,14 +109,13 @@ d3.json(queryUrl).then(function(response){
   });
 
   legend.onAdd = function(myMap){
-    var div = L.DomUtil.create('div','legend');
-    labels = ['<strong>Depth</strong>']
+    var div = L.DomUtil.create('div','legend'),
+    labels = ['<strong>Depth</strong>'],
     categories = ['-10-10','10-30','30-50','50-70','70-90','90+'];
-    colors = ['#00FF00','#B3FF00','#F0FF00','#FFB100','#FF8300','#FF1300']
+    colors = ['#00FF00','#B3FF00','#F0FF00','#FFB100','#FF8300','#FF1300'];
     for (var i=0; i < categories.length; i++) {
       div.innerHTML += 
-      '<i style ="background:' + colors[i] + '"></i>' +
-      categories[i] + (categories[i+1] ? '&ndash;' + categories[i+1] + '<br>': '+');
+      '<i style ="background:' + getColor(categories[i]+1) + '"></i>' + categories[i] + (categories[i+1] ? '&ndash;' + categories[i+1] + '<br>': '+');
     }
     //div.innerHTML = labels.join('</br>')
     return div;
